f3bfcee713f3d275ad182fb3ac72da014f2db158
Shefat Ullah <147303858+shefatullah-ndub@users.noreply.github.com>
2023-11-11 22:05:32 +0600

    Add files via upload

---
+++ knn/README.md
@@ -0,0 +1,17 @@
+# TensorFlow
+
+### Setup Environment
+Create an isolated conda environment
+
+```
+conda create --name tensorenv python=3.11
+```
+Activate the environment
+```
+conda activate tensorenv
+```
+Setup python interpreter in PyCharm and start installing the requirements
+```
+pip install -r requirements.tx
+```
+Finally, run corresponding notebook file
---
+++ knn/knn.py
@@ -0,0 +1,42 @@
+#import numpy as np
+#import pandas as pd
+#pytorch interpreter
+
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_blobs
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+X, y = make_blobs(n_samples = 500, n_features = 2, centers = 4,cluster_std = 1.5, random_state = 4)
+
+plt.style.use('dark_background')
+plt.figure(figsize = (10,10))
+plt.scatter(X[:,0], X[:,1], c=y, marker= '*',s=100,edgecolors='black')
+plt.show()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+
+knn5 = KNeighborsClassifier(n_neighbors = 5)
+knn1 = KNeighborsClassifier(n_neighbors=1)
+
+
+knn5.fit(X_train, y_train)
+knn1.fit(X_train, y_train)
+
+y_pred_5 = knn5.predict(X_test)
+y_pred_1 = knn1.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy with k=5", accuracy_score(y_test, y_pred_5)*100)
+print("Accuracy with k=1", accuracy_score(y_test, y_pred_1)*100)
+
+plt.figure(figsize=(15, 5))
+plt.subplot(1, 2, 1)
+plt.scatter(X_test[:, 0], X_test[:, 1], c=y_pred_5, marker='*', s=100, edgecolors='black')
+plt.title("Predicted values with k=5", fontsize=20)
+
+plt.subplot(1, 2, 2)
+plt.scatter(X_test[:, 0], X_test[:, 1], c=y_pred_1, marker='*', s=100, edgecolors='black')
+plt.title("Predicted values with k=1", fontsize=20)
+plt.show()
